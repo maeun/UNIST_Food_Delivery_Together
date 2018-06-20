5f93d2deabff409b1b4e02b79b6d20ac61db17f8
maeun <maeuniyee@naver.com>
2018-06-20 23:51:11 +0900

    DetailActivity에서는 Firebase에서 연락처 가져오도록 구현 중

--- app/src/main/java/com/maeun/unist_food_delivery_together/DetailActivity.kt
+++ app/src/main/java/com/maeun/unist_food_delivery_together/DetailActivity.kt
@@ -3,9 +3,18 @@ package com.maeun.unist_food_delivery_together
 import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.activity_write.*
+import org.json.JSONObject
 
 class DetailActivity : AppCompatActivity() {
 
@@ -18,16 +27,60 @@ class DetailActivity : AppCompatActivity() {
         detail_writer_tv.setText(intent.getStringExtra("writer"))
         detail_time_tv.setText(intent.getStringExtra("time"))
 
-        val storage = FirebaseStorage.getInstance().getReference()
-        val storageRef: StorageReference = storage.child("menu").child(intent.getStringExtra("category") + "/" + intent.getStringExtra("restaurant"))
+//        val storage = FirebaseStorage.getInstance().getReference()
+//        val storageRef: StorageReference = storage.child("menu").child(intent.getStringExtra("category") + "/" + intent.getStringExtra("restaurant"))
+//
+//        val ONE_MEGABYTE = (1024 * 1024).toLong()
+//
+//        //download file as a byte array
+//        storageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
+//            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+//            detail_menu_iv.setImageBitmap(bitmap)
+//        }
+//                .addOnFailureListener{
+//
+//                }
 
-        val ONE_MEGABYTE = (1024 * 1024).toLong()
+        val databasereference = FirebaseDatabase.getInstance().getReference()
 
-        //download file as a byte array
-        storageRef.getBytes(ONE_MEGABYTE).addOnSuccessListener { bytes ->
-            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-            detail_menu_iv.setImageBitmap(bitmap)
-        }
+        databasereference.child("restaurant").child(intent.getStringExtra("category")).child(intent.getStringExtra("restaurant"))
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
 
-    }
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+                        var jsonObject = JSONObject(dataSnapshot.getValue().toString())
+
+                        var i: Iterator<String> = jsonObject.keys()
+
+                        while (i.hasNext()) {
+                            var key: String = i.next()
+
+                            if (key == "contact") {
+                                detail_contact_tv.setText(jsonObject.getString(key))
+                            }
+                        }
+                    }
+                })
+
+
+
+
+//        databasereference.child("restaurant").child(intent.getStringExtra("category")).child(intent.getStringExtra("restaurant"))
+//                .addValueEventListener(object : ValueEventListener {
+//            override fun onCancelled(p0: DatabaseError?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//
+//                Log.d("aaa",dataSnapshot.toString())
+//
+//                var contact: PartyItem? = dataSnapshot.getValue(PartyItem::class.java)
+//                contact = PartyItem(contact!!.contact)
+//                detail_contact_tv.setText(contact.toString())
+            }
+
 }
